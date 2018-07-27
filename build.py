2c11d73abe357b45aa02eab4c4b09d4b8ee4449e
notskm <41784014+notskm@users.noreply.github.com>
2018-07-27 13:15:15 -0400

    Initial Commit

---
+++ build.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from bincrafters import build_template_default
+import copy
+
+def is_not_bad_build(build):
+    if build.options['sfml:graphics'] or build.options['sfml:audio']:
+        if build.settings['compiler'] == 'gcc' and build.settings['compiler.version'] == '8':
+            return False
+        if build.settings['compiler'] == 'clang' and build.settings['compiler.version'] == '6.0':
+            return False
+    return True
+
+def add_build_requires(builds):
+    return map(add_required_installers, builds)
+
+def add_required_installers(build):
+    installers = ['ninja_installer/1.8.2@bincrafters/stable']
+    build.build_requires.update({"*": installers})
+    return build
+
+if __name__ == "__main__":
+
+    builder = build_template_default.get_builder()
+
+    for build in builder.items:
+        build.options['sfml:window'] = False
+        build.options['sfml:graphics'] = False
+        build.options['sfml:audio'] = False
+        build.options['sfml:network'] = False
+
+    builds = list(builder.items)
+    for option in ['sfml:window', 'sfml:graphics', 'sfml:audio', 'sfml:network']:
+        new_builds = copy.deepcopy(builds)
+        for build in new_builds:
+            build.options[option] = True
+        builder.items.extend(new_builds)
+    
+    builder.items = add_build_requires(builder.items)
+    
+    # Some dependencies don't have gcc-8 or clang-6.0 binaries, so some SFML libs fail to build
+    builder.builds = filter(is_not_bad_build, builder.items)
+
+builder.run()
---
+++ conanfile.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools
+import os
+
+
+class SfmlConan(ConanFile):
+    name = 'sfml'
+    version = '2.5.0'
+    description = 'Simple and Fast Multimedia Library'
+    url = 'https://github.com/bincrafters/conan-sfml'
+    homepage = 'https://github.com/SFML/SFML'
+    author = 'Bincrafters <bincrafters@gmail.com>'
+    license = 'zlib/png'
+
+    exports = ['LICENSE.md']
+
+    exports_sources = ['CMakeLists.txt']
+    generators = 'cmake'
+
+    settings = 'os', 'compiler', 'build_type', 'arch'
+    options = {
+        'shared': [True, False],
+        'fPIC': [True, False],
+        'window': [True, False],
+        'graphics': [True, False],
+        'network': [True, False],
+        'audio': [True, False],
+    }
+    default_options = 'shared=False', 'fPIC=True', 'window=False', 'graphics=False', 'network=False', 'audio=False'
+
+    source_subfolder = 'source_subfolder'
+    build_subfolder = 'build_subfolder'
+
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
+    
+    def configure(self):
+        if self.options.graphics:
+            self.options.window = True
+
+    def requirements(self):
+        if self.settings.os == 'Linux':
+            if self.options.graphics:
+                self.requires.add('freetype/2.9.0@bincrafters/stable')
+            if self.options.audio:
+                self.requires.add('openal/1.18.2@bincrafters/stable')
+                self.requires.add('flac/1.3.2@bincrafters/stable')
+                self.requires.add('ogg/1.3.3@bincrafters/stable')
+                self.requires.add('vorbis/1.3.6@bincrafters/stable')
+    
+    def system_requirements(self):
+        if self.settings.os == 'Linux' and tools.os_info.is_linux:
+            if tools.os_info.with_apt:
+                installer = tools.SystemPackageTool()
+                if self.settings.arch == 'x86':
+                    arch_suffix = ':i386'
+                elif self.settings.arch == 'x86_64':
+                    arch_suffix = ':amd64'
+
+                packages = ['pkg-config%s' % arch_suffix]
+
+                if self.options.window:
+                    packages.extend(['libx11-dev%s' % arch_suffix])
+                    packages.extend(['libxrandr-dev%s' % arch_suffix])
+                    packages.extend(['libudev-dev%s' % arch_suffix])
+                    packages.extend(['libgl1-mesa-dev%s' % arch_suffix])
+
+                for package in packages:
+                    installer.install(package)
+
+    def source(self):
+        source_url = 'https://github.com/SFML/SFML'
+        tools.get('{0}/archive/{1}.tar.gz'.format(source_url, self.version))
+        extracted_dir = 'SFML-' + self.version
+        os.rename(extracted_dir, self.source_subfolder)
+        tools.replace_in_file(self.source_subfolder + '/cmake/Modules/FindFLAC.cmake',
+                            'find_library(FLAC_LIBRARY NAMES FLAC)',
+                            'find_library(FLAC_LIBRARY NAMES flac)')
+        tools.replace_in_file(self.source_subfolder + '/cmake/Modules/FindFreetype.cmake', 'libfreetype', 'libfreetype\n    freetyped')
+
+    def configure_cmake(self):
+        cmake = CMake(self, generator='Ninja')
+        cmake.definitions['SFML_DEPENDENCIES_INSTALL_PREFIX'] = self.package_folder
+        cmake.definitions['SFML_MISC_INSTALL_PREFIX'] = self.package_folder
+
+        cmake.definitions['SFML_BUILD_WINDOW'] = self.options.window
+        cmake.definitions['SFML_BUILD_GRAPHICS'] = self.options.graphics
+        cmake.definitions['SFML_BUILD_NETWORK'] = self.options.network
+        cmake.definitions['SFML_BUILD_AUDIO'] = self.options.audio
+
+        if self.settings.os == 'Windows':
+            if self.settings.compiler.runtime == 'MT' or self.settings.compiler.runtime == 'MTd':
+                cmake.definitions['SFML_USE_STATIC_STD_LIBS'] = True
+
+        if self.settings.os == 'Linux':
+            library_prefix_paths = ''
+            if self.options.graphics:
+                library_prefix_paths += self.deps_cpp_info['freetype'].rootpath + ';'
+            if self.options.audio:
+                library_prefix_paths += self.deps_cpp_info['flac'].rootpath + ';'
+                library_prefix_paths += self.deps_cpp_info['vorbis'].rootpath + ';'
+                library_prefix_paths += self.deps_cpp_info['ogg'].rootpath + ';'
+                library_prefix_paths += self.deps_cpp_info['openal'].rootpath + ';'
+            cmake.definitions['CMAKE_PREFIX_PATH'] = library_prefix_paths
+
+        if self.settings.os != 'Windows':
+            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+        cmake.configure(build_folder=self.build_subfolder, source_folder=self.source_subfolder)
+        return cmake
+
+    def build(self):
+        if self.settings.compiler == 'Visual Studio':
+            with tools.vcvars(self.settings, force=True, filter_known_paths=False):
+                cmake = self.configure_cmake()
+        else:
+            cmake = self.configure_cmake()
+        cmake.build()
+    
+    def package(self):
+        self.copy(pattern='License.md', dst='licenses', src=self.source_subfolder)
+        cmake = self.configure_cmake()
+        cmake.install()
+    
+    def add_libraries_from_pc(self, library, static=None):
+        if static is None:
+            static = not self.options.shared
+        pkg_config = tools.PkgConfig(library, static=static)
+        libs = [lib[2:] for lib in pkg_config.libs_only_l]  # cut -l prefix
+        lib_paths = [lib[2:] for lib in pkg_config.libs_only_L]  # cut -L prefix
+        self.cpp_info.libs.extend(libs)
+        self.cpp_info.libdirs.extend(lib_paths)
+        self.cpp_info.sharedlinkflags.extend(pkg_config.libs_only_other)
+        self.cpp_info.exelinkflags.extend(pkg_config.libs_only_other)
+
+    def package_info(self):
+        self.cpp_info.defines = ['SFML_STATIC'] if not self.options.shared else []
+
+        suffix = '-s' if not self.options.shared else ''
+        suffix += '-d' if self.settings.build_type == 'Debug' else ''
+        sfml_main_suffix = '-d' if self.settings.build_type == 'Debug' else ''
+
+        if self.options.graphics:
+            self.cpp_info.libs.append('sfml-graphics' + suffix)
+        if self.options.window:
+            self.cpp_info.libs.append('sfml-window' + suffix)
+        if self.options.network:
+            self.cpp_info.libs.append('sfml-network' + suffix)
+        if self.options.audio:
+            self.cpp_info.libs.append('sfml-audio' + suffix)
+        if self.settings.os == 'Windows':
+            self.cpp_info.libs.append('sfml-main' + sfml_main_suffix)
+        self.cpp_info.libs.append('sfml-system' + suffix)
+
+        if not self.options.shared:
+            if self.settings.os == 'Windows':
+                if self.options.graphics:
+                    self.cpp_info.libs.append('freetype')
+                if self.options.window:
+                    self.cpp_info.libs.append('opengl32')
+                    self.cpp_info.libs.append('gdi32')
+                if self.options.audio:
+                    self.cpp_info.libs.append('openal32')
+                    self.cpp_info.libs.append('flac')
+                    self.cpp_info.libs.append('vorbisenc')
+                    self.cpp_info.libs.append('vorbisfile')
+                    self.cpp_info.libs.append('vorbis')
+                    self.cpp_info.libs.append('ogg')
+                if self.options.network:
+                    self.cpp_info.libs.append('ws2_32')
+                self.cpp_info.libs.append('winmm')
+            elif self.settings.os == 'Linux':
+                if self.options.window:
+                    self.add_libraries_from_pc('xrandr')
+                if self.options.graphics:
+                    self.cpp_info.libs.append('GL')
+                    self.cpp_info.libs.append('udev')
