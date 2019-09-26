1e07f5b25536e2f787e2db083ab5bb6ea5488a27
SSE4 <tomskside@gmail.com>
2019-09-26 18:40:05 +0700

    - test all CMake-based generators
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,21 +7,25 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "xcode"
+    generators = "cmake", "cmake_find_package", "cmake_find_package_multi", "xcode"
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions['WITH_WINDOW'] = self.options['sfml'].window
-        cmake.definitions['WITH_GRAPHICS'] = self.options['sfml'].graphics
-        cmake.definitions['WITH_AUDIO'] = self.options['sfml'].audio
-        cmake.definitions['WITH_NETWORK'] = self.options['sfml'].network
-        cmake.configure()
-        cmake.build()
+        for generator in ["cmake", "cmake_targets", "cmake_find_package", "cmake_find_package_multi"]:
+            self.output.info("test generator %s" % generator)
+            cmake = CMake(self)
+            cmake.definitions['CONAN_CMAKE_GENERATOR'] = generator
+            cmake.definitions['WITH_WINDOW'] = self.options['sfml'].window
+            cmake.definitions['WITH_GRAPHICS'] = self.options['sfml'].graphics
+            cmake.definitions['WITH_AUDIO'] = self.options['sfml'].audio
+            cmake.definitions['WITH_NETWORK'] = self.options['sfml'].network
+            cmake.configure(build_folder=generator)
+            cmake.build()
         if tools.is_apple_os(self.settings.os):
             xcodeproj = os.path.join(self.source_folder, "conan-sfml-test", "conan-sfml-test.xcodeproj")
             xcconfig = os.path.join(self.build_folder, "conanbuildinfo.xcconfig")
             self.run('xcodebuild -configuration conanbuildinfo -xcconfig "%s" -project "%s"' % (xcconfig, xcodeproj))
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        self.run(bin_path, run_environment=True)
+        for generator in ["cmake", "cmake_targets", "cmake_find_package", "cmake_find_package_multi"]:
+            bin_path = os.path.join(generator, "bin", "test_package")
+            self.run(bin_path, run_environment=True)
