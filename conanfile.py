8733e94d2ad9ca5355bed89fd8c242d9b5c58cd1
bincrafters-user <bincrafters@gmail.com>
2019-12-08 05:07:48 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.15.0

--- conanfile.py
+++ conanfile.py
@@ -46,7 +46,7 @@ class SfmlConan(ConanFile):
             self.requires.add('freetype/2.9.1@bincrafters/stable')
             self.requires.add('stb/20180214@conan/stable')
         if self.options.audio:
-            self.requires.add('openal/1.19.0@bincrafters/stable')
+            self.requires.add('openal/1.19.1')
             self.requires.add('flac/1.3.2@bincrafters/stable')
             self.requires.add('ogg/1.3.4')
             self.requires.add('vorbis/1.3.6@bincrafters/stable')
